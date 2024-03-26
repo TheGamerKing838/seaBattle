58095c2b83b8dc1fe87071cbf340d4a4e35496a4
TheGamerKing838 <102060990+TheGamerKing838@users.noreply.github.com>
2024-03-26 23:41:48 +0530

    Incomplete Version
    Only intro, game actually doesn't exist, you may complete it.

---
+++ main.py
@@ -0,0 +1,12 @@
+count1 = 5
+count2 = 5
+
+print("Welcome to Sea Battle!!!")
+print("You get 5 ships, Ship A, B, C, D, E")
+print("Deploy them on a Block, for Ex: 1, 2, 3, 4, ....16")
+print("PLAYER 2, PLEASE GO OUT OF THE ROOM (NO CHEATING!!!)")
+print("Player 1's turn, You have", count1, "ships left")
+p1ShipA = input("Choose a block to deploy Ship A: ")
+if 0 < p1ShipA < 17():
+
+
